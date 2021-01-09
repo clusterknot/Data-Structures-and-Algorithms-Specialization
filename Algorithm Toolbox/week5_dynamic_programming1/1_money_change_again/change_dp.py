367b3aab519842d7b4949acccfdae23462bc84cb
alok-ds-blackbuck <alokanand@blackbuck.com>
2021-01-09 19:34:38 +0530

    Week4 solution

--- Algorithm Toolbox/week5_dynamic_programming1/1_money_change_again/change_dp.py
+++ Algorithm Toolbox/week5_dynamic_programming1/1_money_change_again/change_dp.py
@@ -3,8 +3,24 @@ import sys
 
 def get_change(m):
     #write your code here
-    return m // 4
+    if m==0:
+        return 0
+    table = [0 for i in range(m + 1)]
+    table[0] = 0
+    for i in range(1, m + 1):
+        table[i] = sys.maxsize
+    coins = [1,3,4]
+    for i in range(1, m + 1):
+        for j in range(len(coins)):
+            if (coins[j] <= i):
+                sub_res = table[i - coins[j]]
+                if (sub_res != sys.maxsize and
+                    sub_res + 1 < table[i]):
+                    table[i] = sub_res + 1
+    return table[m]
+
 
 if __name__ == '__main__':
     m = int(sys.stdin.read())
+    # m  = int(input())
     print(get_change(m))
