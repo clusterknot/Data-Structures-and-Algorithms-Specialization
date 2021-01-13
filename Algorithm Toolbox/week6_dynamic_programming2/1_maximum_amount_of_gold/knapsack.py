ae2bf71b2fdd6721ce8d55bcd76b21970ca5fbb6
alok-ds-blackbuck <alokanand@blackbuck.com>
2021-01-13 16:17:58 +0530

    Course completed

--- Algorithm Toolbox/week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.py
+++ Algorithm Toolbox/week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.py
@@ -1,15 +1,27 @@
 # Uses python3
 import sys
 
-def optimal_weight(W, w):
-    # write your code here
-    result = 0
-    for x in w:
-        if result + x <= W:
-            result = result + x
-    return result
+def maxGold(W, n, items):
+    """ Outputs the maximum weight of gold that fits in knapsack of capacity W
+    (int, int, list) -> (int, 2D-array) """
+
+    value = [[0 for _ in range(n+1)] for _ in range(W+1)]
+    for i in range(1, W+1):
+        for j in range(1, n+1):
+            # if item i is not part of optimal knapsack
+            value[i][j] = value[i][j-1]
+            if items[j-1]<=i:
+                # if item i is part of optimal knapsack
+                temp = value[i-items[j-1]][j-1] + items[j-1]
+                # max(i in knapsack, i not in knapsack)
+                if temp > value[i][j]:
+                    value[i][j] = temp
+
+    return (int(value[W][n]))
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
+    # input = input()
     W, n, *w = list(map(int, input.split()))
-    print(optimal_weight(W, w))
+    print(maxGold(W,n, w))
--- Algorithm Toolbox/week6_dynamic_programming2/2_partitioning_souvenirs/partition3.py
+++ Algorithm Toolbox/week6_dynamic_programming2/2_partitioning_souvenirs/partition3.py
@@ -1,20 +1,34 @@
-# Uses python3
+# # Uses python3
 import sys
 import itertools
 
-def partition3(A):
-    for c in itertools.product(range(3), repeat=len(A)):
-        sums = [None] * 3
-        for i in range(3):
-            sums[i] = sum(A[k] for k in range(len(A)) if c[k] == i)
+def partition3(W, n, items):
+    """ Finds if number of partitions having capacity W is >=3
+    (int, int, list) -> (int) """
+    count = 0 
+    value = [[0 for x in range(n + 1)] for y in range(W + 1)]
+    for i in range(1, W+1):
+        for j in range(1, n+1):
+            value[i][j] = value[i][j-1]
+            if items[j-1]<=i:
+                temp = value[i-items[j-1]][j-1] + items[j-1]
+                if temp > value[i][j]:
+                    value[i][j] = temp
+            if value[i][j] == W: count += 1
 
-        if sums[0] == sums[1] and sums[1] == sums[2]:
-            return 1
-
-    return 0
+    if count < 3: return '0'
+    else: return '1'
 
 if __name__ == '__main__':
     input = sys.stdin.read()
+    # input = input()
     n, *A = list(map(int, input.split()))
-    print(partition3(A))
+    total_weight = sum(A)
+    if n<3: 
+        print('0')
+    elif total_weight%3 != 0: 
+        print('0')
+    else:
+        print(partition3(total_weight//3, n, A))
+    # print(partition3(A))
 
